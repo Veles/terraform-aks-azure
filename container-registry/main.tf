d3f0ab24e37516ff1f0e1da22dcf2f9562991785
Veles <derunix@ya.ru>
2022-10-01 15:01:33 +0300

    rg

--- container-registry/main.tf
+++ container-registry/main.tf
@@ -15,8 +15,8 @@ resource "azurerm_container_registry" "acr" {
 
 resource "azurerm_kubernetes_cluster" "kube" {
   name                = "example-kube"
-  location            = azurerm_resource_group.kube.location
-  resource_group_name = azurerm_resource_group.kube.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "exampleaks1"
 
   default_node_pool {
