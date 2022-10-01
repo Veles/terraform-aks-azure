eecc1d939baebc3f72561b43d499e1130086bad7
Veles <derunix@ya.ru>
2022-10-01 13:40:09 +0300

    fix

--- container-registry/backend.tf
+++ container-registry/backend.tf
@@ -24,7 +24,5 @@ variable "client_secret" {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = "723f9d92-2b27-4228-9238-abac464e81cd"
-  tenant_id       = "176af66e-3d60-4d7b-8af4-550dc1364cd7"
-  client_id       = "67af739c-2635-4265-9822-aeb7d847dfbd"
+
 }
