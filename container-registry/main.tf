c96355073f63e289e57322007bf363664312a5ae
Veles <derunix@ya.ru>
2022-10-01 13:59:32 +0300

    asdasd

--- container-registry/main.tf
+++ container-registry/main.tf
@@ -5,8 +5,6 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-
-
 resource "azurerm_container_registry" "acr" {
   name                = var.registryname
   resource_group_name = azurerm_resource_group.rg.name
