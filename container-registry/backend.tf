03d1d00a8a613aa1c3e080155457f0d0ddc3c01a
Veles <derunix@ya.ru>
2022-10-01 13:45:14 +0300

    ad

--- container-registry/backend.tf
+++ container-registry/backend.tf
@@ -21,11 +21,12 @@ variable "client_secret" {
   type = string
   sensitive = true
 }
-variable "tenant_id" {
+
+variable "subscription_id" {
   type = string
   sensitive = true
 }
-variable "subscription_id" {
+variable "client_id" {
   type = string
   sensitive = true
 }
